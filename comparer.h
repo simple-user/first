f5411990c31566811a3d67e2a6dbfd92c455335a
san <san@host.com>
2015-12-07 22:29:48 +0200

    Add strinf variable to comparer.h

--- comparer.h
+++ comparer.h
@@ -7,31 +7,33 @@ using std::string;
 class comparer
 {
 public:
-	typedef vec<string> vecS;
-	comparer(string tfile, string folder);
+        typedef vec<string> vecS;
+        comparer(string tfile, string folder);
 
-	bool load();
-	bool isUniqueLists();
-	bool compareToCMD();
-	void printContainersToCMD();
-	string getError()
-	{
-		return errorLoading;
-	}
+        bool load();
+        bool isUniqueLists();
+        bool compareToCMD();
+        void printContainersToCMD();
+        string getError()
+        {
+                return errorLoading;
+        }
 private:
-	string tFile;
-	string folder;
-	
-	size_t countInList;
-	size_t countInFolder;
-	
-	string errorLoading;
-	
-	vec<string> vecTextFile;
-	vec<string> vecFolder;
-	vec<string> vecRes;
+        string tFile;
+        string folder;
+        string Name;
 
-	void myIntersection();
-	bool isUnique(vecS &test);
+
+        size_t countInList;
+        size_t countInFolder;
+
+        string errorLoading;
+
+        vec<string> vecTextFile;
+        vec<string> vecFolder;
+        vec<string> vecRes;
+
+        void myIntersection();
+        bool isUnique(vecS &test);
 };
 
